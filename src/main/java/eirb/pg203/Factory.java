433a16b91d0b7194cb67f6ac7fa908ddee636bca
AMRAR RAFIQ <ramrar@enseirb-matmeca.fr>
2025-12-05 05:30:54 +0100

    fixed bug

--- src/main/java/eirb/pg203/Factory.java
+++ src/main/java/eirb/pg203/Factory.java
@@ -1,6 +1,8 @@
 package eirb.pg203;
 import java.util.List;
 import java.util.Map;
+import java.util.HashMap;
+import java.util.ArrayList;
 
 public class Factory {
     public static CalElement calElement(Map<String, String> map) {
@@ -14,6 +16,10 @@ public class Factory {
     }
 
     public static Calender calender(List<Map<String,String>> maps) {
+
+        if (maps.isEmpty()) {
+            return new Calender(new HashMap<>(), new ArrayList<>());
+        }
         Map<String, String> header = maps.get(0);
 
         // List<CalElement> els =maps.subList(1, maps.size()) 
