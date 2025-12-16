eff586922299c1b7f855395a8e5117e528dd9030
Soufiane SBAI <soufiane.sbai@bordeaux-inp.fr>
2025-12-16 15:57:20 +0100

    deplacer outputformat to cli

---
+++ src/main/java/eirb/pg203/cli/OutputFormat.java
@@ -0,0 +1,11 @@
+package eirb.pg203.output;
+
+/**
+ * Énumération des formats de sortie.
+ */
+public enum OutputFormat
+{
+  TEXT,   // Sortie standard (console)
+  ICS,    // Format ICS
+  HTML    // Format HTML
+}
