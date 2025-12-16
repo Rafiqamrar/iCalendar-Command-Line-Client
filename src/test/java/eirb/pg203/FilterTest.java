f5f10815ff1d598676cacad4f74aeae9cb3c9816
Mohamed Amine BENKADUR <mohamed_amine.benkadur@bordeaux-inp.fr>
2025-12-16 15:00:16 +0100

    fixes

--- src/test/java/eirb/pg203/FilterTest.java
+++ src/test/java/eirb/pg203/FilterTest.java
@@ -52,4 +52,52 @@ public class FilterTest {
 
     }
 
+    @Test
+    void testFrom_SimpleEventFile(){
+        List<String> args = List.of(
+                "i2.ics", "events", 
+                "-from", "20260101");
+        CliConfig config = CliParser.parse(args.toArray(new String[0]));
+        assertEquals(config.getEventFilter(), EventFilterType.RANGE, "Il faut Spécifier les dates pour les options");
+        Path file = getTestFile("i2.ics");
+        Calendar calendar = parser.parse(file);
+        assertNotNull(calendar, "Calendrier ne devrait pas être null pour i2.ics");
+        List<CalElement> cals = calendar.get(ViewType.EVENTS);
+        List<Event> events = new ArrayList<>();
+
+        for (CalElement cal : cals) {
+            events.add((Event) cal);
+        }
+
+        assertEquals(156, cals.size(), "Devrait avoir 156 EVENT");
+
+        List<Event> filteredEvents = EventFilters.filter(events, config);
+
+        assertEquals(72, filteredEvents.size(), "On doit avoir 16 events");
+    }
+
+    @Test
+    void testTo_SimpleEventFile(){
+        List<String> args = List.of(
+                "i2.ics", "events", 
+                "-to", "20260101");
+        CliConfig config = CliParser.parse(args.toArray(new String[0]));
+        assertEquals(config.getEventFilter(), EventFilterType.RANGE, "Il faut Spécifier les dates pour les options");
+        Path file = getTestFile("i2.ics");
+        Calendar calendar = parser.parse(file);
+        assertNotNull(calendar, "Calendrier ne devrait pas être null pour i2.ics");
+        List<CalElement> cals = calendar.get(ViewType.EVENTS);
+        List<Event> events = new ArrayList<>();
+
+        for (CalElement cal : cals) {
+            events.add((Event) cal);
+        }
+
+        assertEquals(156, cals.size(), "Devrait avoir 156 EVENT");
+
+        List<Event> filteredEvents = EventFilters.filter(events, config);
+
+        assertEquals(84, filteredEvents.size(), "On doit avoir 16 events");
+    }
+
 }
